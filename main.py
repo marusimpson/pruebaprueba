057d46c30a68d12b4aa09a7315fa7166603f0978
marusimpson <163221733+marusimpson@users.noreply.github.com>
2026-05-27 11:05:06 -0300

    Describe el cambio

--- main.py
+++ main.py
@@ -2,12 +2,27 @@
 from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 import argparse
 
-class EditorDeImagen:
-    ''''Clase para editar imágenes utilizando la biblioteca PIL (Pillow).'''
-    def __init__(self, ruta_imagen):
-        '''Inicializa la clase con la ruta de la imagen a editar.'''
-        self.imagen = Image.open(ruta_imagen)
+class cuentabancaria(self,):
+    def __init__(self, titular, saldo_inicial=0):
+        self.titular = titular
+        self.saldo = saldo_inicial
 
+    def depositar(self, cantidad):
+        if cantidad > 0:
+            self.saldo += cantidad
+            print(f"Depósito exitoso. Nuevo saldo: {self.saldo}")
+        else:
+            print("La cantidad a depositar debe ser mayor que cero.")
 
-editor = EditorDeImagen(r'C:\Users\Windows Microsoft\Desktop\trabajo py\Bart_Simpson_200px.png')
-editor.imagen.show()
+    def retirar(self, cantidad):
+        if cantidad > 0:
+            if cantidad <= self.saldo:
+                self.saldo -= cantidad
+                print(f"Retiro exitoso. Nuevo saldo: {self.saldo}")
+            else:
+                print("Fondos insuficientes para realizar el retiro.")
+        else:
+            print("La cantidad a retirar debe ser mayor que cero.")
+
+    def mostrar_saldo(self):
+        print(f"Titular: {self.titular}, Saldo: {self.saldo}")
